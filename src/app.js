a4a277541027c22b08f80d3c72b2b9a49e2428d3
Brennan Ives <brennanives@gmail.com>
2024-06-15 16:12:38 -0400

    added cors to app.js

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
 
 // TODO: Add your code here
+app.use(cors());
 app.use(express.json());
 
 app.use("/movies", moviesRouter);
